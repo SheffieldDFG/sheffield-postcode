d0641ae770f3b1c3b6ee44442c3a4a82793594da
David Jones <drj@pobox.com>
2017-12-02 21:41:53 +0000

    Download from URL

--- shfpc/main.go
+++ shfpc/main.go
@@ -1,4 +1,31 @@
 package main
 
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+)
+
 func main() {
+	download()
+}
+
+func download() {
+	out, err := os.Create("sheffield-postcode.csv")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer out.Close()
+
+	resp, err :=
+		http.Get("http://geoportal.statistics.gov.uk/datasets/75edec484c5d49bcadd4893c0ebca0ff_0.csv")
+	defer resp.Body.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	n, err := io.Copy(out, resp.Body)
+	fmt.Print(n, " bytes copied\n")
 }
