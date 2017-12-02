63219da1962821a697ac386a689e2fce5e080a58
David Jones <drj@pobox.com>
2017-12-02 22:16:02 +0000

    Extract sheffield postcodes

--- shfpc/main.go
+++ shfpc/main.go
@@ -1,19 +1,44 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"io"
 	"log"
 	"net/http"
 	"os"
+	"strings"
 )
 
 func main() {
-	outputName := "all-postcode.csv"
-	if _, err := os.Stat(outputName); os.IsNotExist(err) {
+	dlName := "all-postcode.csv"
+	if _, err := os.Stat(dlName); os.IsNotExist(err) {
 		// path/to/whatever does not exist
-		download(outputName)
+		download(dlName)
 	}
+
+	inp, err := os.Open(dlName)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	out, err := os.Create("sheffield-postcode.csv")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	n := 0
+	lines := bufio.NewScanner(inp)
+	lines.Scan()
+	fmt.Fprintln(out, lines.Text())
+	for lines.Scan() {
+		line := lines.Text()
+		if strings.Contains(line, "E08000019") {
+			fmt.Fprintln(out, lines.Text())
+			n++
+		}
+	}
+	fmt.Println(n, "records written")
 }
 
 func download(name string) {
